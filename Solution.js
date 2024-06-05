65c5c9437c54dcb6aaf7512d303f1bac4e92f15e
Lachezar Kutsarov <lkutsarov@gmail.com>
2024-06-05 20:45:03 +0200

    Create Solution.js

---
+++ Solution.js
@@ -0,0 +1,55 @@
+
+/**
+ * @param {string} compressed
+ * @return {string}
+ */
+var betterCompression = function (compressed) {
+    this.ALPHABET_SIZE = 26;
+    this.ASCII_SMALL_CASE_A = 97;
+    this.ASCII_ZERO = 48;
+
+    const frequency = createArrayFrequency(compressed);
+    return createBetterCompression(frequency);
+};
+
+/**
+ * @param {string} compressed
+ * @return {number[]}
+ */
+function createArrayFrequency(compressed) {
+    const frequency = new Array(this.ALPHABET_SIZE).fill(0);
+    for (let i = 0; i < compressed.length; ++i) {
+
+        let compression = 0;
+        let letter = compressed.codePointAt(i);
+        while (++i < compressed.length && isDigit(compressed.charAt(i))) {
+            compression = compression * 10 + (compressed.codePointAt(i) - this.ASCII_ZERO);
+        }
+        frequency[letter - this.ASCII_SMALL_CASE_A] += compression;
+        --i;
+    }
+    return frequency;
+}
+
+/**
+ * @param {number[]} frequency
+ * @return {string}
+ */
+function createBetterCompression(frequency) {
+    const betterCompression = new Array();
+    for (let letter = 0; letter < this.ALPHABET_SIZE; ++letter) {
+        if (frequency[letter] > 0) {
+            betterCompression.push(String.fromCodePoint(letter + this.ASCII_SMALL_CASE_A));
+            betterCompression.push((frequency[letter]).toString());
+        }
+    }
+    return betterCompression.join('');
+}
+
+/**
+ * @param {string} character
+ * @return {boolean}
+ */
+function isDigit(character) {
+    return '0' <= character && character <= '9';
+}
